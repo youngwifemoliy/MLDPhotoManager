d3cf6c6b804a57a4ff10f089a719629c27b0e89d
Moliy <youngwifemoliy@live.cn>
2017-05-09 10:24:23 +0800

    no message

--- Demo/MLDPhtotManagerDemo/MLDPhotoManager/MLDPhotoManager.h
+++ Demo/MLDPhtotManagerDemo/MLDPhotoManager/MLDPhotoManager.h
@@ -11,11 +11,10 @@
 #import "LGPhoto.h"
 
 @interface MLDPhotoManager : NSObject
-
 /**
  呼出相册控制器
 
- @param sender 控制器的发起者(因为如果是iPad需要一个停靠的view,这个sender就是ipad在呼出的View,比如点击了某个Button)
+ @param carryView 控制器的发起者(因为如果是iPad需要一个停靠的view,这个sender就是ipad在呼出的View,比如点击了某个Button)
  @param cameraImage 输出相机的单张照片(原图)
  @param albumArray 输出相册中多选的照片数组(原图)
  */
